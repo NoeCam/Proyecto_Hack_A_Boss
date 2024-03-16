db07b1766dd7f664a8868d80af06c5328e3d299a
Adrián Conde Pérez <acp641991@gmail.com>
2024-03-16 16:48:36 +0100

    Proyecto base

---
+++ Proyectos/Backups/backup-2/main.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const totalCards = 16;
+let newCards = [];
+let newValues = [];
+let flippedCards = [];
+let movesCounter = 0;
+let playsCounter = 0;
+const emojis = [
+    { id: 1, value: '😉'},
+    { id: 2, value: '😎'},
+    { id: 3, value: '😓'},
+    { id: 4, value: '😮'},
+    { id: 5, value: '😈'},
+    { id: 6, value: '😴'},
+    { id: 7, value: '😵'},
+    { id: 8, value: '🐱'},
+];
+
+
+const game = document.querySelector('.game');
+const totalPlays = document.querySelector('.totalPlays');
+const playAgainButton = document.querySelector('.playAgain');
+const newCardTemplate = `<div class="card"><div class="cardBack">?</div><div class="cardFront"></div></div>`;
+
+//! TODO => función general para la creación de cartas con sus eventos
+//! TODO => lógica para tener 2 cartas con el mismo valor
+//! TODO => lógica para la comprobación de valores
+//! TODO => lógica para tener 2 cartas levantadas
+function printCards(){
+
+    playAgainButton.style.display = 'none';
+    generateCards();
+    addListeners();
+};
+
+function generateCards(){
+
+    for (let i = 0; i < totalCards; i++){
+
+        const div = document.createElement('div');
+        div.innerHTML = newCardTemplate;
+        newCards.push(div);
+        game.append(newCards[i]);
+
+        generateValues();
+
+        newCards[i].querySelector('.cardFront').textContent = newValues[i];
+
+    };
+};
+
+function generateValues(){
+
+    const randomValue = Math.floor(Math.random() * totalCards / 2) + 1;
+    let values = newValues.filter(value => value === randomValue);
+
+    if (values.length < 2){
+        newValues.push(randomValue);
+    } else {
+        generateValues();
+    };
+};
+
+function addListeners(){
+
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => card.addEventListener('click', flipCard));
+
+    playAgainButton.addEventListener('click', playAgain);
+};
+
+function flipCard(e){
+
+    const currentCard = e.currentTarget;
+
+    if (movesCounter == 0){
+
+        currentCard.classList.add('flipped');
+        flippedCards.push(currentCard);
+        movesCounter++;
+    };
+
+    if (movesCounter == 1 && !currentCard.classList.contains('flipped')){
+
+        currentCard.classList.add('flipped');
+        flippedCards.push(currentCard);
+        movesCounter++;
+    };
+
+    if (movesCounter == 2){
+                
+        //! TODO => Intentos +1
+        playsCounter++;
+        totalPlays.innerHTML = `Total de jugadas: ${playsCounter}`;
+
+        if (flippedCards[0].querySelector('.cardFront').innerHTML == flippedCards[1].querySelector('.cardFront').innerHTML){
+
+            flippedCards = [];
+            movesCounter = 0;
+                    
+        } else {
+
+            setTimeout(() => {
+                flippedCards.forEach(card => card.classList.remove('flipped'));
+                flippedCards = [];
+                movesCounter = 0;
+            }, 1000);
+        };
+    };
+
+    //! TODO => lógica para mostrar el botón ???? length flippedCards ????
+    const flippedCardList = document.querySelectorAll('.flipped');
+    if (flippedCardList.length == 16){
+        playAgainButton.style.display = 'block';
+    };
+};
+
+//! TODO => lógica para volver a jugar
+function playAgain(){
+
+    newCards = [];
+    newValues = [];
+    flippedCards = [];
+    movesCounter = 0;
+    playsCounter = 0;
+
+    const cardsToDelete = document.querySelectorAll('.game > div');
+    cardsToDelete.forEach(card => card.remove());
+
+    playAgainButton.style.display = 'none';
+    totalPlays.innerHTML = 'Total de jugadas: 0'
+
+    printCards();
+};
+
+printCards();
